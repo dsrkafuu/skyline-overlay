b1a81fcd784d333c8c604c2044472711025d6598
amzrk2 <amzrk2@outlook.com>
2020-10-17 21:45:17 +0800

    new: add vuex v4

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './index.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import './index.css';
 
-createApp(App).mount('#app')
+import { store } from './store';
+
+const app = createApp(App);
+
+app.use(store);
+
+app.mount('#app');
