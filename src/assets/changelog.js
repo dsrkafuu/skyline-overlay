ab441d7f6f7635a62885895d761304f851c2380c
amzrk2 <amzrk2@outlook.com>
2021-02-14 17:08:28 +0800

    release: v1.1.0

--- src/assets/changelog.js
+++ src/assets/changelog.js
@@ -1,5 +1,10 @@
 export const changelog = [
   {
+    version: 'v1.1.0',
+    date: 1613293607036,
+    notes: ['new: combatent sort settings', 'fix: max hit shadow overflow'],
+  },
+  {
     version: 'v1.0.0',
     date: 1613204843494,
     notes: ['update: initial release', 'refactor: migrate app to reactjs'],
