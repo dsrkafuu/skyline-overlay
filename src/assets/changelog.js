5841e350211f75efb2afb5d16c5312b939bb8e87
amzrk2 <amzrk2@outlook.com>
2021-02-15 15:15:44 +0800

    release: v1.2.0

--- src/assets/changelog.js
+++ src/assets/changelog.js
@@ -1,5 +1,15 @@
 export const changelog = [
   {
+    version: 'v1.2.0',
+    date: 1613373308393,
+    notes: [
+      'new: ui zoom support',
+      'new: classification settings with tabs',
+      'new: save settings to local storage',
+      'fix: settings animation',
+    ],
+  },
+  {
     version: 'v1.1.0',
     date: 1613293607036,
     notes: ['new: combatent sort settings', 'fix: max hit shadow overflow'],
