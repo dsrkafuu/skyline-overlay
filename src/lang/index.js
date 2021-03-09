cdf3ec68eed86298e5a245e6100fa2fea1e7bb34
Slightlyxz <youresenpai@gmail.com>
2021-03-09 06:31:20 +0100

    new: german translation

--- src/lang/index.js
+++ src/lang/index.js
@@ -1,9 +1,11 @@
 import en from './en.json';
 import ja from './ja.json';
 import zh from './zh.json';
+import de from './de.json';
 
 export default {
   en: { translation: en },
   ja: { translation: ja },
   zh: { translation: zh },
+  de: { translation: de },
 };
