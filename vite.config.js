2abccce61bfce42f23bde374cb3118e6ed16d957
呆萌 <admin@diemoe.net>
2021-08-17 06:40:23 +0800

    Relative path support

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import svgr from '@svgr/rollup';
  * https://vitejs.dev/config/
  */
 export default defineConfig({
-  base: process.env.BASE_URL || '/',
+  base: process.env.BASE_URL || './',
   plugins: [
     reactRefresh(),
     // https://react-svgr.com/docs/options/
