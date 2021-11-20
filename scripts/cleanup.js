de16bbfa106280566ca6241b138d672ddd000a39
dsrkafuu <dsrkafuu@outlook.com>
2021-11-20 14:17:09 +0800

    build: release v2.2.1

--- scripts/cleanup.js
+++ scripts/cleanup.js
@@ -1,8 +1,8 @@
 /**
  * [NOTE]
  *
- * the `@svgr/rollup` handles all svg import,
- * which vite still output `.svg` files into `dist/assets`
+ * remove all source code content in sourcemaps
+ * like webpack
  */
 
 const fs = require('fs/promises');
@@ -10,21 +10,48 @@ const path = require('path');
 const glob = require('glob');
 const chalk = require('chalk');
 
-console.log(chalk.blue('cleaning up redudant svg files...'));
+console.log(chalk.blue('removing sourcemap source contents...'));
+
+/**
+ * @param {string} path
+ * @returns {Promise<void>}
+ */
+function removeContent(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, { encoding: 'utf-8' })
+      .then((content) => {
+        const sourcemap = JSON.parse(content);
+        if (!sourcemap.sourcesContent) {
+          resolve();
+        }
+        delete sourcemap.sourcesContent;
+        fs.writeFile(path, JSON.stringify(sourcemap), { encoding: 'utf-8' })
+          .then(() => {
+            resolve();
+          })
+          .catch((e) => {
+            reject(e);
+          });
+      })
+      .catch((e) => {
+        reject(e);
+      });
+  });
+}
 
 const workers = [];
-const svgs = glob.sync('dist/assets/*.svg');
-svgs.forEach((val) => {
+const sourcemaps = glob.sync('dist/assets/*.map');
+sourcemaps.forEach((val) => {
   const p = path.resolve(__dirname, '../', val);
-  workers.push(fs.rm(p, { force: true }));
+  workers.push(removeContent(p));
 });
 
 Promise.all(workers)
   .then(() => {
-    console.log(chalk.green(`redudant ${svgs.length} svg files removed`));
+    console.log(chalk.green(`source contents removed in ${sourcemaps.length} sourcemaps`));
   })
   .catch((e) => {
-    console.log(chalk.red('failed to remove some of svg files'));
+    console.log(chalk.red('failed to process some of sourcemaps'));
     console.error(e);
     process.exitCode = 1;
   });
--- src/assets/version.ts
+++ src/assets/version.ts
@@ -1,2 +1,2 @@
-export const version = 'v2.2.0';
-export const date = 1632377930117;
+export const version = 'v2.2.1';
+export const date = 1637388556130;
