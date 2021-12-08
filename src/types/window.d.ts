e50eab981e001fa9da4ddacd1de14f4470e5e783
dsrkafuu <dsrkafuu@outlook.com>
2021-12-08 14:08:05 +0800

    refactor: remove unused constants

--- src/types/window.d.ts
+++ src/types/window.d.ts
@@ -1,11 +1,7 @@
-import { LANG_KEY } from '../utils/constants';
-import lang from '../lang';
-
 // NGLD's FFXIV Overly
 declare global {
   interface Window {
     OverlayPluginApi: unknown;
-    [LANG_KEY]: keyof typeof lang;
   }
 }
 
