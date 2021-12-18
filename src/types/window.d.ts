5d5d9ee750b012b0f53858b1eb0a51a210aaa207
dsrkafuu <dsrkafuu@outlook.com>
2021-12-18 11:10:16 +0800

    feat: have fun with new structured cloning api

--- src/types/window.d.ts
+++ src/types/window.d.ts
@@ -2,6 +2,8 @@
 declare global {
   interface Window {
     OverlayPluginApi: unknown;
+    // https://developer.mozilla.org/en-US/docs/Web/API/structuredClone
+    structuredClone<T>(src: T): T;
   }
 }
 
--- src/utils/lodash.ts
+++ src/utils/lodash.ts
@@ -1,9 +1,10 @@
-/**
- * simple deep clone
- */
-export function cloneDeep<T>(src: T): T {
-  const map = new Map();
+import { toJS } from 'mobx';
 
+/**
+ * simple deep clone polyfill
+ */
+function cloneDeepPolyfill<T>(src: T): T {
+  const map = new Map();
   function _cloneDeep<T>(src: T): T {
     if (src && typeof src === 'object') {
       if (map.has(src)) {
@@ -20,12 +21,26 @@ export function cloneDeep<T>(src: T): T {
       return src;
     }
   }
-
   const ret = _cloneDeep(src);
   map.clear();
   return ret;
 }
 
+/**
+ * native deep clone or simple polyfill
+ */
+export function cloneDeep<T>(src: T): T {
+  if (typeof window.structuredClone === 'function') {
+    // https://developer.mozilla.org/en-US/docs/Web/API/structuredClone
+    return window.structuredClone(toJS(src));
+  } else {
+    return cloneDeepPolyfill(toJS(src));
+  }
+}
+
+/**
+ * escape simple xss chars
+ */
 export function xssEscape(str: string) {
   return str.replace(/[<>&]/g, (c) => {
     switch (c) {
