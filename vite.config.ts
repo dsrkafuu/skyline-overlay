21832701044bb5f99090f0bccc4740edd7409c59
dsrkafuu <dsrkafuu@outlook.com>
2021-12-30 00:09:22 +0800

    chore: update deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import svgr from '@svgr/rollup';
+import svgr from 'vite-plugin-svgr';
 import htmlEnv from 'vite-plugin-html-env';
 import { visualizer } from 'rollup-plugin-visualizer';
 
@@ -11,13 +11,14 @@ export default defineConfig({
   base: process.env.VITE_BASE_URL || './',
   plugins: [
     svgr({
-      icon: true,
-      typescript: true,
-      svgProps: { className: 'g-icon' },
+      svgrOptions: {
+        icon: true,
+        // typescript: true,
+        svgProps: { className: 'g-icon' },
+      },
     }),
     react(),
     htmlEnv(process.env),
-    visualizer(),
   ],
   css: {
     preprocessorOptions: {
@@ -28,28 +29,29 @@ export default defineConfig({
   },
   build: {
     emptyOutDir: true,
-    // rollupOptions: {
-    //   output: {
-    //     manualChunks(id) {
-    //       // do not split css
-    //       if (
-    //         /\.css$/i.test(id) ||
-    //         /\.s[ac]ss$/i.test(id) ||
-    //         /\.less$/i.test(id)
-    //       ) {
-    //         return;
-    //       }
-    //       // sentry chunk
-    //       if (/node_modules[/\\]@sentry/i.test(id)) {
-    //         return 'sentry';
-    //       }
-    //       // vendor deps chunk
-    //       else if (/node_modules/i.test(id)) {
-    //         return 'vendor';
-    //       }
-    //       // automatic dynamic import spliting should work as is
-    //     },
-    //   },
-    // },
+    rollupOptions: {
+      plugins: [visualizer()],
+      // output: {
+      //   manualChunks(id) {
+      //     // do not split css
+      //     if (
+      //       /\.css$/i.test(id) ||
+      //       /\.s[ac]ss$/i.test(id) ||
+      //       /\.less$/i.test(id)
+      //     ) {
+      //       return;
+      //     }
+      //     // sentry chunk
+      //     if (/node_modules[/\\]@sentry/i.test(id)) {
+      //       return 'sentry';
+      //     }
+      //     // vendor deps chunk
+      //     else if (/node_modules/i.test(id)) {
+      //       return 'vendor';
+      //     }
+      //     // automatic dynamic import spliting should work as is
+      //   },
+      // },
+    },
   },
 });
