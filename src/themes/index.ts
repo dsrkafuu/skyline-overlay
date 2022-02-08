32a1fc05599326eb715fc09a06da15b6d2bdcdc6
Josh <jcjohnson77@gmail.com>
2022-02-08 18:34:08 -0500

    Add round theme

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -1,9 +1,11 @@
 import './default.scss';
 import './horiz.scss';
 import './ikegami.scss';
+import './round.scss';
 
 export default {
   default: { text: 'Skyline Overlay' },
   horiz: { text: 'HORIZOVERLAY' },
   ikegami: { text: 'ikegami' },
+  round: { text: 'Jimmy Round' },
 };
