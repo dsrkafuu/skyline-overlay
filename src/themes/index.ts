7e98d7ed63ba35965ce410cf9de91637558a0157
dsrkafuu <dsrkafuu@outlook.com>
2022-02-09 13:55:49 +0800

    chore: adjust theme details

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -1,11 +1,11 @@
 import './default.scss';
 import './horiz.scss';
 import './ikegami.scss';
-import './round.scss';
+import './jround.scss';
 
 export default {
   default: { text: 'Skyline Overlay' },
   horiz: { text: 'HORIZOVERLAY' },
   ikegami: { text: 'ikegami' },
-  round: { text: 'Jimmy Round' },
+  jround: { text: 'Jimmy Round' },
 };
