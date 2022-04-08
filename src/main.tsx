951ea2e5ed503a756da4b873524b1b8c4a61054b
dsrkafuu <dsrkafuu@outlook.com>
2022-04-08 17:30:57 +0800

    build: optimize dynamic import for tree-shaking

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,20 @@ import './themes';
 import './scss/fonts.scss';
 import './scss/utils.scss';
 
-import * as Sentry from '@sentry/react';
-import { Integrations } from '@sentry/tracing';
-
+// [tree-shakable] dynamic import sentry
 const sentryDsn = import.meta.env.VITE_SENTRY_DSN;
-if (sentryDsn && import.meta.env.PROD) {
-  Sentry.init({
-    dsn: `${sentryDsn}`,
-    integrations: [new Integrations.BrowserTracing()],
-    sampleRate: 1, // report all errors
-    tracesSampleRate: 0.05, // report 5% of traces
-  });
+if (import.meta.env.PROD && sentryDsn) {
+  Promise.all([import('@sentry/react'), import('@sentry/tracing')]).then(
+    ([Sentry, { Integrations }]) => {
+      console.log(Sentry, Integrations);
+      Sentry.init({
+        dsn: `${sentryDsn}`,
+        integrations: [new Integrations.BrowserTracing()],
+        sampleRate: 1, // report all errors
+        tracesSampleRate: 0.05, // report 5% of traces
+      });
+    }
+  );
 }
 
 import { StrictMode } from 'react';
@@ -25,7 +28,7 @@ import App from './App';
 import DevPanel from './DevPanel';
 
 let app = <App />;
-// should be tree-shaked in production by vite
+// [tree-shakable] development panel
 if (import.meta.env.DEV && !isCEFSharp()) {
   app = <DevPanel>{app}</DevPanel>;
 }
