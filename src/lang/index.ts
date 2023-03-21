09667ebd844c1aa337dacc747c29e3d4d01a7f4d
Emily Maxwell <elestriel@gmail.com>
2023-03-21 16:41:40 +0900

    Added French translations

--- src/lang/index.ts
+++ src/lang/index.ts
@@ -3,6 +3,7 @@ import ja from './ja.json';
 import zh from './zh.json';
 // import de from './de.json';
 import ko from './ko.json';
+import fr from './fr.json';
 
 export interface TranslationData {
   [key: string]: string | undefined;
@@ -14,4 +15,5 @@ export default {
   zh: { text: zh.LANG, translation: zh as TranslationData },
   // de: { text: de.LANG, translation: de as TranslationData },
   ko: { text: ko.LANG, translation: ko as TranslationData },
+  fr: { text: fr.LANG, translation: fr as TranslationData }
 };
