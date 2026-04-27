0706d6d3d39e80b2bbbb67cb0d5f1037db524574
DSRKafuU <21136293+dsrkafuu@users.noreply.github.com>
2026-04-27 14:20:49 +0800

    chore: DevPanel no longer needed

--- src/main.tsx
+++ src/main.tsx
@@ -4,20 +4,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 
-import { isCEFSharp } from './api';
 import App from './App';
-import DevPanel from './DevPanel';
 import ErrorBoundary from './ErrorBoundary';
 import { store } from './store';
 
 let app = <App />;
 
-// [tree-shakable] development panel
-if (import.meta.env.DEV && !isCEFSharp()) {
-  app = <DevPanel>{app}</DevPanel>;
-}
-
-// mount the app
 const root = document.getElementById('root');
 if (root) {
   createRoot(root).render(
